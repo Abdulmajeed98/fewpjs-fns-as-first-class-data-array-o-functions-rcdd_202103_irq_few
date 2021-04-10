5b7b286b3a4e2b60c3402b998cb69c6afdd4d238
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-10 09:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function wakeDog (dogName, dogBreed){
   console.log(str);
   return str;
 }
+
+function leashDog (dogName, dogBreed){
+  let str = `Leash ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
